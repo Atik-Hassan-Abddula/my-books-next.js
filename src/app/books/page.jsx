7d528e7bbe7a7d94be0331a7159d6a2b2b49186d
Atik-Hasan-55 <atik508030@gmail.com>
2026-09-22 12:10:23 +0600

    card add

--- src/app/books/page.jsx
+++ src/app/books/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import BooksCard from '../Components/BooksCard';
 
-const Bookspage = () => {
+const getBooks = async ()=>{
+    const res = await fetch('http://localhost:5000/books')
+    return res.json()
+}
+
+const Bookspage = async () => {
+    const books = await getBooks()
     return (
         <div>
-            <h1>books page</h1>
+            <h1>books page {books.length} </h1>
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    books.map(books=><BooksCard key={books.id} books={books}></BooksCard>)
+                }
+            </div>
         </div>
     );
 };
